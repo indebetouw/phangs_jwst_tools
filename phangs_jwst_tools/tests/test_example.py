eb672c809881b6adbacbf6504a4424d068084ffc
Erik Rosolowsky <rosolowsky@ualberta.ca>
2024-07-25 19:55:18 +0000

    Dumbest test

--- phangs_jwst_tools/tests/test_example.py
+++ phangs_jwst_tools/tests/test_example.py
@@ -1,8 +1,8 @@
 
 
 def test_primes():
-    from ..example_mod import primes
-    assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    from ..complim import *
+    assert 3 == 3
 
 
 def test_deprecation():
