374fe28ad1496e33d2e7fc1d8ccc287e4e6f0507
Erik Rosolowsky <rosolowsky@ualberta.ca>
2024-07-25 19:43:08 +0000

    Strip out dumb cython dependency

--- phangs_jwst_tools/__init__.py
+++ phangs_jwst_tools/__init__.py
@@ -7,8 +7,8 @@ from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
 __all__ = []
-from .example_mod import *   # noqa
+from .complim import *   # noqa
 # Then you can be explicit to control what ends up in the namespace,
-__all__ += ['do_primes']   # noqa
+__all__ += ['fake_image', 'phot_catalog', 'completeness_analysis', 'completeness_limit']   # noqa
 # or you can keep everything from the subpackage with the following instead
 # __all__ += example_mod.__all__
